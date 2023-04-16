34ed3f12a68fdda10373d7386e1f363e602e23dd
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2023-04-16 00:00:39 -0400

    Change landing page CSS to match other pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,47 @@
 <section
-	class="relative bg-[url(https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80)] bg-cover bg-center bg-no-repeat"
+	class="flex items-center justify-center min-h-screen pattern-dots pattern-gray-200 pattern-bg-gray-100 pattern-size-4 opacity-100 sm:p-8"
 >
 	<div
-		class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"
-	/>
-
-	<div
-		class="relative mx-auto max-w-screen-xl min-h-screen px-4 py-32 sm:px-6 flex items-center lg:px-8"
+		class="rounded-2xl shadow-xl overflow-hidden relative bg-[url(https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80)] bg-cover bg-center bg-no-repeat"
 	>
-		<div class="max-w-xl text-center sm:text-left">
-			<h1 class="text-3xl font-extrabold sm:text-5xl">
-				Learn with
+		<div
+			class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"
+		/>
 
-				<strong class="block font-extrabold text-lime-700"> Sustainability Inc. </strong>
-			</h1>
+		<div
+			class="relative mx-auto sm:min-w-[64rem] h-96 sm:h-[40rem] px-4 py-32 sm:px-6 flex items-center lg:px-8"
+		>
+			<div class="max-w-xl text-center sm:text-left">
+				<h1 class="text-3xl font-extrabold sm:text-5xl">
+					Learn with
 
-			<p class="mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
-				An educational game that teaches you about the environment. As the CEO of Sustainability
-				Inc., your decisions will determine the future of the planet.
-			</p>
+					<strong class="block font-extrabold text-lime-700"> Sustainability Inc. </strong>
+				</h1>
 
-			<div class="mt-8 flex flex-wrap gap-4 text-center">
-				<a
-					href="/play"
-					class="block w-full rounded bg-lime-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-lime-700 focus:outline-none focus:ring active:bg-lime-500 sm:w-auto"
-				>
-					Get Started
-				</a>
+				<p class="mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+					An educational game that teaches you about the environment. As the CEO of Sustainability
+					Inc., your decisions will determine the future of the planet.
+				</p>
 
-				<a
-					href="/info"
-					class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-lime-600 shadow hover:text-lime-700 focus:outline-none focus:ring active:text-lime-500 sm:w-auto"
-				>
-					Learn More
-				</a>
+				<div class="mt-8 flex flex-wrap gap-4 text-center">
+					<a
+						href="/play"
+						class="block w-full rounded bg-lime-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-lime-700 focus:outline-none focus:ring active:bg-lime-500 sm:w-auto"
+					>
+						Get Started
+					</a>
+
+					<a
+						href="/info"
+						class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-lime-600 shadow hover:text-lime-700 focus:outline-none focus:ring active:text-lime-500 sm:w-auto"
+					>
+						Learn More
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
-	<p class="absolute right-4 bottom-4 text-xs font-medium">
-		By playing this game, you agree to have cookies stored on your device.
+	<p class="absolute right-4 bottom-4 text-xs font-medium text-gray-400">
+		By continuing to use this site, you agree to have cookies stored on your device.
 	</p>
 </section>
