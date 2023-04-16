6b6888e7de01fd3650da618d9a0b266587e96717
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2023-04-16 11:17:45 -0400

    Add info page

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,90 @@
+<section
+	class="flex items-center justify-center min-h-screen pattern-dots pattern-gray-200 pattern-bg-gray-100 pattern-size-4 opacity-100 sm:p-8"
+>
+	<div class="relative leading-5 mx-auto max-w-screen-xl p-8 bg-white rounded-2xl shadow-xl">
+		<h1 class="font-bold text-2xl mt-4">About Sustainability Inc.</h1>
+		<h2 class="font-medium text-lg mt-4">Inspiration</h2>
+		<p>
+			Sustainability Inc. is inspired by games like <a
+				class="text-blue-300"
+				href="https://orteil.dashnet.org/cookieclicker/">Cookie Clicker</a
+			>, where you sell cookies by clicking on them. Through this project, I wanted to use the
+			popularity of this style of game to educate people on the carbon emissions of everyday items.
+		</p>
+		<h2 class="font-medium text-lg mt-4">What it does</h2>
+		<p>
+			In Sustainability Inc., you&#39;re the CEO of a small company that sells different products.
+			The goal of the game is to see how much carbon emissions you are producing when selling each
+			item to progress. The game is designed to be educational, so you can click on any item to
+			learn more about it.
+		</p>
+		<h2 class="font-medium text-lg mt-4">How we built it</h2>
+		<ol class="list-decimal ml-4">
+			<li>Set up a new SvelteKit Project</li>
+			<li>
+				Import <a class="text-blue-300" href="https://tailwindcss.com/">TailwindCSS</a> and other dependencies
+			</li>
+			<li>
+				Design and implement the UI
+				<ul class="list-disc ml-8">
+					<li>
+						Some of the UI elements are variations of the <a
+							class="text-blue-300"
+							href="https://www.hyperui.dev/">HyperUI Components</a
+						>
+					</li>
+				</ul>
+			</li>
+			<li>Implement the game logic, including buying and selling items</li>
+			<li>Add the carbon emissions data</li>
+			<li>
+				Add a tutorial for people who are new to the game, including a cookie so you only see it
+				once
+			</li>
+			<li>
+				Make description pages for each item with more information, like:
+				<ul class="list-disc ml-8">
+					<li>What the item is, and what it&#39;s used for</li>
+					<li>Information about the carbon emissions</li>
+					<li>
+						Other concerns about the item, like the use of child labor or the use of toxic chemicals
+					</li>
+				</ul>
+			</li>
+		</ol>
+		<h2 class="font-medium text-lg mt-4">Challenges we ran into</h2>
+		<ol>
+			<li>
+				Different sources of carbon emissions data all had slightly different numbers, so I had to
+				do a lot of research to find the most accurate numbers.
+			</li>
+			<li>
+				Staying consistent with the style of the game while also sourcing images that I was legally
+				allowed to use was a challenge. I ended up using a lot of images from <a
+					class="text-blue-300"
+					href="https://unsplash.com/">Unsplash</a
+				>, which are all licensed under the
+				<a class="text-blue-300" href="https://unsplash.com/license">Unsplash License</a>.
+			</li>
+		</ol>
+		<h2 class="font-medium text-lg mt-4">Accomplishments that we&#39;re proud of</h2>
+		<p>
+			Getting the game to work in addition to researching and implementing the carbon emissions data
+			was a big accomplishment. I&#39;m also proud of the UI, which I think is very clean and easy
+			to use.
+		</p>
+		<h2 class="font-medium text-lg mt-4">What we learned</h2>
+		<p>
+			I learned a lot about the carbon emissions of everyday items, and I learned a lot about the
+			scale of difference between different items. I hope players of the game will learn a lot about
+			the carbon emissions of everyday items as I did, and I hope they will be inspired to make more
+			sustainable choices.
+		</p>
+		<h2 class="font-medium text-lg mt-4">What&#39;s next for Sustainability Inc.</h2>
+		<p>
+			I would like to add more items to the game, and I would like to add a way to compare your
+			carbon emissions to the average person in your country. I would also like to move the page to
+			a more permanent domain.
+		</p>
+	</div>
+</section>
